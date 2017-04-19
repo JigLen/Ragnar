bf1939e926825a0c5625cb16f42455bbc9233d20
Ciaran <ciaran2293@yahoo.com>
2017-04-19 16:01:26 +0100

    The user now doesnt have access to the whole site only to what he/she is allowed to see. This is done in the access check script and some edits to the new bootstrap index

--- src/main/webapp/js/access_check.js
+++ src/main/webapp/js/access_check.js
@@ -7,3 +7,27 @@ function checkAccess(access_lv,user_access){
     }
     return false;
 }
+function permissions(user_access){
+    var sidebar=document.getElementById("sidebar")
+    //buttons.removeChild(log_in);
+    if(user_access !=1 && user_access !=4  ){
+        document.getElementById("first_line_quries").remove();;
+
+    }
+
+    if(user_access == 3){
+        document.getElementById("investigating_problems").style.visibility="hidden";
+    }
+    else if(user_access ==1){
+        document.getElementById("userstory7").remove();
+        document.getElementById("userstory9").remove()
+        document.getElementById("userstory14").remove();
+    }
+    else if(user_access ==2){
+        document.getElementById("userstory8").remove();
+    }
+    if(user_access !=4){
+        document.getElementById("evaluation").remove();
+    }
+
+}
